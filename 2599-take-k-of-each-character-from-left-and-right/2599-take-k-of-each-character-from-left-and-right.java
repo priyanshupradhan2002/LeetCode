ff58cbd55325b8e4612aab79cef2345e76ffcbc0
priyanshupradhan2002 <107298053+priyanshupradhan2002@users.noreply.github.com>
2024-11-20 10:15:48 +0530

    Time: 6 ms (100%), Space: 45.1 MB (83.43%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.java
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.java
@@ -1,27 +1,85 @@
 class Solution {
     public int takeCharacters(String s, int k) {
-        // Total counts
-        int[] count = new int[3];
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
+        int[] charCount = new int[3];
+        char[] chars = s.toCharArray();
+        int length = chars.length;
+        
+        int left;
+        for (left = 0; left < length; left++) {
+            charCount[chars[left] - 'a']++;
+            if (isValidCount(charCount, k)) {
+                break;
+            }
         }
         
-        if (Math.min(Math.min(count[0], count[1]), count[2]) < k) {
+        if (left == length) {
             return -1;
         }
         
-        // Sliding Window
-        int res = Integer.MAX_VALUE;
-        int l = 0;
-        for (int r = 0; r < s.length(); r++) {
-            count[s.charAt(r) - 'a']--;
+        int currentCount = left + 1;
+        int minCount = currentCount;
+        int right = length - 1;
+        
+        while (left >= 0) {
+            int currentChar = chars[left] - 'a';
             
-            while (Math.min(Math.min(count[0], count[1]), count[2]) < k) {
-                count[s.charAt(l) - 'a']++;
-                l++;
+            if (charCount[currentChar] == k) {
+                while (chars[left] != chars[right]) {
+                    charCount[chars[right] - 'a']++;
+                    right--;
+                    currentCount++;
+                }
+                right--;
+            } else {
+                charCount[currentChar]--;
+                currentCount--;
+                minCount = Math.min(currentCount, minCount);
             }
-            res = Math.min(res, s.length() - (r - l + 1));
+            left--;
         }
-        return res;
+        
+        return minCount;
+    }
+    
+    private boolean isValidCount(int[] count, int k) {
+        return count[0] >= k && count[1] >= k && count[2] >= k;
     }
 }
+
+
+/* Simplest alternative 
+
+class Solution {
+    public int takeCharacters(String s, int k) {
+        int[] freq = new int[3];
+        int n = s.length();
+        
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        
+        if (freq[0] < k || freq[1] < k || freq[2] < k) {
+            return -1;
+        }
+        
+        int[] curr = new int[3];
+        int maxLen = 0;
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            curr[s.charAt(right) - 'a']++;
+            
+            while (left <= right && (curr[0] > freq[0] - k || 
+                   curr[1] > freq[1] - k || 
+                   curr[2] > freq[2] - k)) {
+                curr[s.charAt(left) - 'a']--;
+                left++;
+            }
+            
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        
+        return n - maxLen;
+    }
+}
+*/
