45d3a3aed91ac50a7ae3b319a1f6cc98c802ff2d
priyanshupradhan2002 <107298053+priyanshupradhan2002@users.noreply.github.com>
2024-12-24 11:00:52 +0530

    Time: 97 ms (71.65%), Space: 115 MB (68.5%) - LeetHub

--- 3439-find-minimum-diameter-after-merging-two-trees/3439-find-minimum-diameter-after-merging-two-trees.java
+++ 3439-find-minimum-diameter-after-merging-two-trees/3439-find-minimum-diameter-after-merging-two-trees.java
@@ -1,59 +1,44 @@
-
-
 class Solution {
-    private void build(List<List<Integer>> adj, int[][] edges) {
-        for (int[] edge : edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
-        }
-    }
-
-    private void get(int[] p, List<List<Integer>> adj, int node, int dis, boolean[] vis) {
-        int d = p[1];
-        vis[node] = true;
-        if (dis > d) {
-            p[0] = node;
-            p[1] = dis;
-        }
-        for (int neighbor : adj.get(node)) {
-            if (!vis[neighbor]) {
-                get(p, adj, neighbor, dis + 1, vis);
-            }
-        }
-    }
+    private int dia;
 
     public int minimumDiameterAfterMerge(int[][] edges1, int[][] edges2) {
-        int n = edges1.length + 1;
-        int m = edges2.length + 1;
-        List<List<Integer>> adj1 = new ArrayList<>();
-        List<List<Integer>> adj2 = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj1.add(new ArrayList<>());
-        for (int i = 0; i < m; i++) adj2.add(new ArrayList<>());
-
-        build(adj1, edges1);
-        build(adj2, edges2);
-
-        int[] p1 = new int[]{-1, Integer.MIN_VALUE};
-        boolean[] vis = new boolean[n];
-        get(p1, adj1, 0, 0, vis);
-
-        int[] p2 = new int[]{-1, Integer.MIN_VALUE};
-        Arrays.fill(vis, false);
-        get(p2, adj1, p1[0], 0, vis);
-        int d1 = p2[1];
-
-        p1 = new int[]{-1, Integer.MIN_VALUE};
-        vis = new boolean[m];
-        get(p1, adj2, 0, 0, vis);
-
-        p2 = new int[]{-1, Integer.MIN_VALUE};
-        Arrays.fill(vis, false);
-        get(p2, adj2, p1[0], 0, vis);
-        int d2 = p2[1];
-
-        if (edges1.length == 0) d1 = 0;
-        if (edges2.length == 0) d2 = 0;
-
-        return Math.max(Math.max(d1, d2), (d1 + 1) / 2 + (d2 + 1) / 2 + 1);
+        int n = edges1.length + 1, m = edges2.length + 1;
+        List<Integer>[] t1 = new List[n], t2 = new List[m];
+        Arrays.setAll(t1, i -> new ArrayList<>());
+        Arrays.setAll(t2, i -> new ArrayList<>());
+        for (int[] e : edges1) {
+            int u = e[0], v = e[1];
+            t1[u].add(v);
+            t1[v].add(u);
+        }
+        for (int[] e : edges2) {
+            int u = e[0], v = e[1];
+            t2[u].add(v);
+            t2[v].add(u);
+        }
+        dia = 0;
+        diameterOf(t1, 0, -1);
+        int dia1 = dia;
+        dia = 0;
+        diameterOf(t2, 0, -1);
+        int dia2 = dia;
+        return Math.max(Math.max(dia1, dia2), (dia1 + 1) / 2 + 1 + (dia2 + 1) / 2);
     }
+
+    private int diameterOf(List<Integer>[] tree, int i, int pa) {
+        int max1 = 0, max2 = 0;
+        for (int j : tree[i]) {
+            if (j == pa) continue;
+            int d = diameterOf(tree, j, i);
+            if (max1 <= d) {
+                max2 = max1;
+                max1 = d;
+            } else if (max2 < d) {
+                max2 = d;
+            }
+        }
+        dia = Math.max(dia, max1 + max2);
+        return max1 + 1;
+    }
+
 }
