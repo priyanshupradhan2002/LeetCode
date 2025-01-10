9604cdda0304c7a7035936d295f5e007d5dc8caa
priyanshupradhan2002 <107298053+priyanshupradhan2002@users.noreply.github.com>
2025-01-10 09:38:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count distinct elements in every window/count-distinct-elements-in-every-window.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.HashMap;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            ArrayList<Integer> array = new ArrayList<>();
+
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int k = Integer.parseInt(br.readLine());
+
+            ArrayList<Integer> ans = new Solution().countDistinct(arr, k);
+
+            for (Integer val : ans) System.out.print(val + " ");
+            System.out.println();
+
+            t--;
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    ArrayList<Integer> countDistinct(int arr[], int k) 
+    {
+        ArrayList<Integer> al=new ArrayList<>();
+       for(int i=0;i<=arr.length-k;i++)
+       {
+           Set<Integer> s=new HashSet<>();
+           int z=i;
+            int count=0;
+           while(count<k)
+           {
+               s.add(arr[z]);
+               count++;
+               z++;
+           }
+           al.add(s.size());
+            s=null;
+       }
+       return al;
+    }
+}
