b8ee94dba21122f55f0f26e70ca1f58f15b257e6
priyanshupradhan2002 <107298053+priyanshupradhan2002@users.noreply.github.com>
2025-01-16 16:29:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    // Function to find the days of buying and selling stock for max profit.
+    int stockBuySell(int arr[]) 
+    {
+        // int min=Integer.MAX_VALUE;
+        // int max=Integer.MIN_VALUE;
+        int maxProfit=0;
+        int n=arr.length;
+       for (int i = 1; i < n; i++) {
+            // If the current price is greater than the previous price, add the difference
+            if (arr[i] > arr[i - 1]) {
+                maxProfit += arr[i] - arr[i - 1];
+            }
+        }
+        return maxProfit;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.stockBuySell(arr);
+
+            System.out.println(res);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
