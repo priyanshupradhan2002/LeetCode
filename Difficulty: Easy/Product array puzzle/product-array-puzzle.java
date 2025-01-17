b422d56c1f75efd68cf75d276a27ba03bc2594be
priyanshupradhan2002 <107298053+priyanshupradhan2002@users.noreply.github.com>
2025-01-17 10:45:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = new int[n];
+            ans = ob.productExceptSelf(arr);
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static int[] productExceptSelf(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+
+        // Step 1: Fill res with prefix products
+        res[0] = 1; // First element has no prefix
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * arr[i - 1];
+        }
+
+        // Step 2: Multiply suffix products directly into res
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= suffix;
+            suffix *= arr[i];
+        }
+
+        return res;
+    }
+}
