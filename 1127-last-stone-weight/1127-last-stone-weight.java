201208cc14b832f0fa43b1954d739ced70078c16
priyanshupradhan2002 <107298053+priyanshupradhan2002@users.noreply.github.com>
2025-01-18 15:25:58 +0530

    Time: 1 ms (98.87%), Space: 41.9 MB (7.68%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.java
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -1,25 +1,39 @@
 class Solution {
     public int lastStoneWeight(int[] stones) 
     {
-       if(stones.length==1)
-        return stones.length; 
-        ArrayList<Integer> ls=new ArrayList<>();
-        int x=0;
-        for(int i:stones)
-        {
-            ls.add(i);
-        }
-        int n=ls.size();
-        while((n-1)!=0)
-        {
-            Collections.sort(ls);
-             x=ls.get(n-1)-ls.get(n-2);
-            ls.remove(ls.get(n-1));
-            ls.remove(ls.get(n-2));
-            ls.add(x);
-            n--;
+    //    if(stones.length==1)
+    //     return stones.length; 
+    //     ArrayList<Integer> ls=new ArrayList<>();
+    //     int x=0;
+    //     for(int i:stones)
+    //     {
+    //         ls.add(i);
+    //     }
+    //     int n=ls.size();
+    //     while((n-1)!=0)
+    //     {
+    //         Collections.sort(ls);
+    //          x=ls.get(n-1)-ls.get(n-2);
+    //         ls.remove(ls.get(n-1));
+    //         ls.remove(ls.get(n-2));
+    //         ls.add(x);
+    //         n--;
 
+    //     }
+    //     return x;
+
+        int n=stones.length;
+        while(n>1){
+            Arrays.sort(stones);
+            if(stones[n-1]== stones[n-2]){
+                n=n-2;
+            }else{
+                stones[n-2]=stones[n-1]-stones[n-2];
+                n--;
+            }
         }
-        return x;
+        return n == 1 ? stones[0]:0;
+
     }
 }
+   
