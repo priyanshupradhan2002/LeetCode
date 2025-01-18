03357bda84b1486061ee2040395fb001a1354606
priyanshupradhan2002 <107298053+priyanshupradhan2002@users.noreply.github.com>
2025-01-18 12:32:26 +0530

    Time: 0 ms (100%), Space: 42.4 MB (39.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,32 +1,32 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // int i = m - 1; // Pointer for the last element in nums1's actual elements
-        // int j = n - 1; // Pointer for the last element in nums2
-        // int index = m + n - 1; // Pointer for the last position in nums1
+        int i = m - 1; // Pointer for the last element in nums1's actual elements
+        int j = n - 1; // Pointer for the last element in nums2
+        int index = m + n - 1; // Pointer for the last position in nums1
 
-        // // Merge nums1 and nums2 from the end
-        // while (i >= 0 && j >= 0) {
-        //     if (nums1[i] > nums2[j]) {
-        //         nums1[index] = nums1[i];
-        //         i--;
-        //     } else {
-        //         nums1[index] = nums2[j];
-        //         j--;
-        //     }
-        //     index--;
-        // }
-
-        // // If there are remaining elements in nums2
-        // while (j >= 0) {
-        //     nums1[index] = nums2[j];
-        //     j--;
-        //     index--;
-        // }
-
-        for(int i=0;i<nums2.length;i++)
-        {
-            nums1[m+i]=nums2[i];
+        // Merge nums1 and nums2 from the end
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[index] = nums1[i];
+                i--;
+            } else {
+                nums1[index] = nums2[j];
+                j--;
+            }
+            index--;
         }
-        Arrays.sort(nums1);
+
+        // If there are remaining elements in nums2
+        while (j >= 0) {
+            nums1[index] = nums2[j];
+            j--;
+            index--;
+        }
+
+        // for(int i=0;i<nums2.length;i++)
+        // {
+        //     nums1[m+i]=nums2[i];
+        // }
+        // Arrays.sort(nums1);
     }
 }
