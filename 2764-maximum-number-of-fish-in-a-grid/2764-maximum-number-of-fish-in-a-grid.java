b8beb84861dacf5a2b8d23b830fdab24e2ee9393
priyanshupradhan2002 <107298053+priyanshupradhan2002@users.noreply.github.com>
2025-01-28 10:16:49 +0530

    Time: 4 ms (77.03%), Space: 45.2 MB (43.7%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
@@ -1,42 +1,41 @@
 class Solution {
-    // Helper function for DFS
-    private int dfs(int[][] grid, boolean[][] visited, int r, int c, int[][] directions) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        if (r < 0 || r >= m || c < 0 || c >= n || visited[r][c] || grid[r][c] == 0) {
-            return 0;
-        }
-
-        visited[r][c] = true; // Mark the cell as visited
-        int totalFish = grid[r][c]; // Catch fish in the current cell
-
-        // Explore all adjacent cells
-        for (int[] dir : directions) {
-            totalFish += dfs(grid, visited, r + dir[0], c + dir[1], directions);
-        }
-        return totalFish;
-    }
-
     public int findMaxFish(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n]; // To track visited cells
+
         int maxFish = 0;
-
-        // Direction vectors for moving up, right, down, left
-        int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-
-        // Iterate through all cells in the grid
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] > 0 && !visited[i][j]) {
-                    // Start DFS from the water cell
-                    maxFish = Math.max(maxFish, dfs(grid, visited, i, j, directions));
-                }
+        
+        for (int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                
+                if (grid[i][j] > 0) {
+                
+                    int fishInRegion = bfs(grid, i, j);
+                
+                    maxFish = Math.max(maxFish, fishInRegion);
+            }
+                
             }
         }
-
+        
         return maxFish;
+        
+    }
+
+    public int bfs(int[][] grid, int i, int j){
+        
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0){
+            return 0;
+        }
+        
+        int fish = grid[i][j];
+
+    
+        grid[i][j] = 0;
+        fish += bfs(grid, i + 1, j);
+        fish += bfs(grid, i - 1, j);
+        fish += bfs(grid, i, j + 1);
+        fish += bfs(grid, i, j - 1);
+
+        
+        return fish;
     }
 }
